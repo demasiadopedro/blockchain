8a9876b15b266a06576a697843990a84d9eb01f4
demasiadopedro <thepedrosantos@proton.me>
2026-05-06 23:52:15 -0300

    feat: implementação do blockchain class

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -1,12 +1,20 @@
-export interface Bloco{
-    header : {
-        nonce: number
-        hashBloco: string
-    }
-    payload: {
-        sequencia: number
-        timestamp: number
-        data: any
-        hashAnterior: string
+import { Bloco } from "./block.interface";
+
+export class Blockchain{
+   #chain: Bloco[] = [];
+   private prefixoPow = '0';
+   constructor(private readonly dificuldade= 4){
+        this.#chain.push(this.criarBlocoGenesis())
+   }
+    private criarBlocoGenesis(): Bloco {
+        const payload = {
+            sequencia: 0,
+            timestamp: Number(new Date()),
+            dados: 'esse é o primeiro bloco',
+            hashAnterior:'',
+        }
+        throw new Error("Function not implemented.");
     }
 }
+
+
